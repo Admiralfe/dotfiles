85e1f6af1e3a10dc79af850f4221ba04e8d6e28c
Felix Liu <felix.shi.liu@gmail.com>
2022-11-12 14:03:41 +0100

    Add neovim autocompletion with tab

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -16,8 +16,14 @@ vim.opt.clipboard = "unnamedplus"
 
 vim.g.mapleader = '-'
 
+--Match trailing whitespace
+vim.cmd([[match DiffDelete /\s\+$/]])
+
 require("plugins")
 
+local luasnip = require("luasnip")
+
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
 local cmp = require("cmp")
 cmp.setup({
     snippet = {
@@ -30,7 +36,28 @@ cmp.setup({
         { name = "luasnip" },
     }, {
         { name = "buffer" },
-    })
+    }),
+    mapping = {
+        ["<CR>"] = cmp.mapping.confirm { select = true },
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible()  then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, {"i", "s"}),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, {"i","s"}),
+    },
 })
 
 require("lsp_setup")
